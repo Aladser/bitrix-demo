15705e61b0a32a9d01d0498e7fd5707aee1070a7
Aladser <aladser@gmail.com>
2024-04-12 16:20:03 +0900

    Revert "update init"
    This reverts commit 184159396ce7f7354110b574726f7a00d4782cf3.

--- bitrix/managed_cache/MYSQL/orm_b_iblock_property/02/024d3e31f0ad8f9fcfabceee2db759c9.php
+++ bitrix/managed_cache/MYSQL/orm_b_iblock_property/02/024d3e31f0ad8f9fcfabceee2db759c9.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001712906169';
-$dateexpire = '001712992569';
+$datecreate = '001712905219';
+$dateexpire = '001712991619';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{i:0;a:24:{s:2:"ID";s:2:"12";s:9:"IBLOCK_ID";s:1:"7";s:4:"NAME";s:8:"Цена";s:6:"ACTIVE";s:1:"Y";s:4:"SORT";s:3:"500";s:4:"CODE";s:5:"PRICE";s:13:"DEFAULT_VALUE";s:0:"";s:13:"PROPERTY_TYPE";s:1:"S";s:9:"ROW_COUNT";s:1:"1";s:9:"COL_COUNT";s:2:"30";s:9:"LIST_TYPE";s:1:"L";s:8:"MULTIPLE";s:1:"N";s:6:"XML_ID";N;s:9:"FILE_TYPE";s:0:"";s:12:"MULTIPLE_CNT";s:1:"5";s:14:"LINK_IBLOCK_ID";s:1:"0";s:16:"WITH_DESCRIPTION";s:1:"N";s:10:"SEARCHABLE";s:1:"N";s:9:"FILTRABLE";s:1:"N";s:11:"IS_REQUIRED";s:1:"N";s:7:"VERSION";s:1:"1";s:9:"USER_TYPE";N;s:18:"USER_TYPE_SETTINGS";s:6:"a:0:{}";s:4:"HINT";s:0:"";}}}';
 return true;
 ?>
